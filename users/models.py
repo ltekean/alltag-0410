2a272870dcee06cf36516a8838e7f5679d6265e1
woongpang <jr.woong@gmail.com>
2023-04-14 11:49:37 +0900

    all_users 내 pofile_img db 주석처리

--- users/models.py
+++ users/models.py
@@ -9,9 +9,5 @@ class UserModel(AbstractUser):
         db_table = "all_users"
 
     nickname = models.CharField(max_length=20, default="None")
-<<<<<<< HEAD
-    profile_img = models.FileField(upload_to='../static/img/') # 마이페이지, 메인페이지에 보여줄 프로필 이미지 db
-=======
     # profile_img = models.ImageField(null=True, upload_to="", blank=True)
->>>>>>> 294d29d (all_users 내 pofile_img db 주석처리)
     follow = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followee")
