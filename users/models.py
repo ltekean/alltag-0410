a949ab0ce9c53427505e4c49453572072e7c1030
woongpang <jr.woong@gmail.com>
2023-04-14 11:27:52 +0900

    all_users db 내 profile_img 추가

--- users/models.py
+++ users/models.py
@@ -9,4 +9,5 @@ class UserModel(AbstractUser):
         db_table = "all_users"
 
     nickname = models.CharField(max_length=20, default="None")
+    profile_img = models.FileField(upload_to='../static/img/') # 마이페이지, 메인페이지에 보여줄 프로필 이미지 db
     follow = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followee")
